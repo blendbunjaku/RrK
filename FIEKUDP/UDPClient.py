b0fdbf57e09f92c4b49ba9cd84cd042fc5622a36
blendbunjaku <blendbunjaku8@gmail.com>
2017-04-18 21:11:57 +0100

    Update UDPClient.py

--- FIEKUDP/UDPClient.py
+++ FIEKUDP/UDPClient.py
@@ -1,2 +1,19 @@
-import socket
-import threading
+from socket import *
+import datetime
+import time
+import random
+
+
+server = "127.0.0.1"
+port = 9000
+clientSocket = socket(family = AF_INET, type=SOCK_DGRAM)
+print("Kerkesat qe mund te beni : IP , PORT, PRINTO, KENO, KONVERTO, HOST, TIME, FAKTORIEL, ZANORET")
+while True:
+    kerkesa=input("Shkruaj kerkesen : ")
+    
+    clientSocket.sendto(kerkesa.encode("ASCII"),(server,port))
+    if kerkesa=="KONVERTO":
+         Opcioni=input("Opcioni: ")
+         clientSocket.sendto(Opcioni.encode("ASCII"),(server,port))
+    pergjigja , serverAddress=clientSocket.recvfrom(1024)
+    print("Pergjigja: " + pergjigja.decode("ASCII"))
